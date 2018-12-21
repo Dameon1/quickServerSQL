814192b3fbd143a41099d7cdeb7214da91fa5995
Dameon Mendoza <dameonmendoza@gmail.com>
2018-12-21 08:41:25 -0700

    push to travis CI

--- db/seedData.js
+++ db/seedData.js
@@ -1,7 +1,7 @@
 'use strict';
 let knex = require('../knex');
 let util = require('util');
-let exec = util.promisify(require('child_process').exec());
+let exec = util.promisify(require('child_process').exec);
 //let exec = Promise.resolve(require('child_process').exec);
 
 module.exports = function(file){
